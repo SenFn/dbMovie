9a5e91ecf61f439d385e7fe177452ba8931e03a7
Sen <hoangnhan26212@gmail.com>
2022-01-06 20:02:57 +0700

    change type of resquest

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const app = express();
 
 const PORT =  process.env.PORT ||3000;
 app.get("/index", async(req,res) => {
-    return res.json(require("./db.json"));
-})
+    if(req.query == "") return res.json(); ;
 
-app.get("/theloai", async(req,res) => {
-    return res.json(require("./types.json"));
-})
+    if(req.query.page == "home")
+        return res.json(require("./db.json"));
 
-app.get("/trending", async(req,res) => {
-    return res.json(require("./trending.json"));
+    if(req.query.page == "theloai")
+        return res.json(require("./types.json"));
+
+    if(req.query.page == "trending")
+        return res.json(require("./trending.json")); 
+    return res.json(); ;
 })
 
 app.listen(PORT, () => console.log(`Started server at ${PORT}!`));
