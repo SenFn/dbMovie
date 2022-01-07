67ee3d749a5dda205b3ff9df874489e4ff27fe2c
Sen <hoangnhan26212@gmail.com>
2022-01-07 13:17:58 +0700

    update database to mongodb

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
 const express = require('express');
 const app = express();
 
+const { MongoClient } = require('mongodb');
+
+// URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+// Database Name
+const dbName = 'DoAn';
+
+
+async function Connect() {
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    return "Done."
+}
+
+Connect()
+  .then(console.log)
+  .catch(console.error);
+
+async function GetDataByCollection(collectionName){
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    let findResult = await collection.find({}).toArray();
+    return findResult;
+}
+
 const PORT =  process.env.PORT ||3003;
 app.get("/index", async(req,res) => {
     if(req.query == "") return res.json(); ;
+    let dataRes;
 
-    if(req.query.page == "home")
-        return res.json(require("./db.json"));
+    if(req.query.page == "home"){
+        dataRes = await GetDataByCollection("moviedb");
+        return res.json(dataRes);
+    }        
 
-    if(req.query.page == "theloai")
-        return res.json(require("./types.json"));
+    if(req.query.page == "theloai"){
+        dataRes = await GetDataByCollection("types");
+        return res.json(dataRes[0]);
+    }
 
-    if(req.query.page == "trending")
-        return res.json(require("./trending.json")); 
-    return res.json(); ;
+    if(req.query.page == "trending"){
+        dataRes = await GetDataByCollection("trending");
+        return res.json(dataRes);
+    }
+    return res.json();
 })
 
 app.listen(PORT, () => console.log(`Started server at ${PORT}!`));
