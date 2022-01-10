a3a8358c751fb9657e4b7a0a96f79f54fbc11f52
Sen <hoangnhan26212@gmail.com>
2022-01-10 14:10:58 +0700

    test push

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const app = express();
+var app = express();
+
+app.use(express.json());
 
 const { MongoClient } = require('mongodb');
 
@@ -29,7 +31,12 @@ async function GetDataByCollection(collectionName){
     return findResult;
 }
 
+async function AddDataByCollection(collectionName, jsonData){
+    
+}
+
 const PORT =  process.env.PORT ||3003;
+
 app.get("/index", async(req,res) => {
     if(req.query == "") return res.json(); ;
     let dataRes;
@@ -50,5 +57,41 @@ app.get("/index", async(req,res) => {
     }
     return res.json();
 })
+
+app.post('/import/:typeData',async function(req, res){
+
+    if(req.params.typeData.toLowerCase() == "moviedb"){
+        //add data if null id
+        //or edit with existing id
+        if(req.body._id == undefined){
+            // add
+            if(req.body.name == undefined || req.body.director == undefined || req.body.description == undefined || req.body.type == undefined || req.body.release == undefined || req.body.country == undefined || req.body.url == undefined || req.body.rawImg == undefined){
+                return res.json({status: false,message: "field missing"});   
+            }else{
+                return res.json({status: true,message: AddDataByCollection("moviedb", req.body)});               
+            }
+        }else{
+            //check _id are exist TODO
+            //else
+            return res.json({status: false,message: "cannot find data by _id " + req.body._id}); 
+        }   
+    }
+
+    if(req.params.typeData.toLowerCase() == "types"){
+        //add new type of movie
+    }
+    if(req.params.typeData.toLowerCase() == "trending"){
+        //change new list trending by list id movies
+    }
+
+    if(req.params.typeData.toLowerCase() == "users"){
+        //add new user
+        //if existing user change name
+        return res.json({status: true,message: req.body}); 
+    }
+
+    console.log(req.body.name);      // your JSON
+    res.send(req.body);    // echo the result back
+});
 
 app.listen(PORT, () => console.log(`Started server at ${PORT}!`));
